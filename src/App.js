bb05e8a68193ae94be7d804041f9f86bb9fe85eb
Arber Shkurti <arbershkurti@yahoo.com>
2024-01-07 20:39:23 +0100

    .

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <img src='./image1.jpg'/>
       </header>
     </div>
